8c024cc3fd63c239cf31a5243a1c37d68f6ecef2
xavier <xavier@whoisstudio.com>
2020-11-11 19:12:10 -0500

    Completed variables

---
+++ main.js
@@ -0,0 +1,12 @@
+// alert('Outputing information.');
+var age = 31; // Number literal
+var name = 'John Hopkins'; // string literal
+var isCreditCardValid = true; // boolean
+var empty = null; // Clears
+var creditcardNumber;
+
+console.log(age);
+console.log(name);
+console.log(isCreditCardValid);
+console.log(empty);
+console.log(creditcardNumber);
